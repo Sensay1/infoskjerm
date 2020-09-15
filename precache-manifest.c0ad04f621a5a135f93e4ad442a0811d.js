68b1d353cb3626c5c7dace3a67684371ee023054
Gerhard Gustavsen <gerhard.gustavsen@outlook.com>
2020-09-15 14:09:12 +0200

    Updates

---
+++ precache-manifest.c0ad04f621a5a135f93e4ad442a0811d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7e1e0bbe73d15288542169cef87c0",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "9b0ee85fb72981c57101",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f3b7bb45247b9dfe82a",
+    "url": "/infoskjerm/static/js/2.73791c32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.73791c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0ee85fb72981c57101",
+    "url": "/infoskjerm/static/js/main.d4751fd3.chunk.js"
+  },
+  {
+    "revision": "8549882e00c44ec97c00",
+    "url": "/infoskjerm/static/js/runtime-main.fe85ec15.js"
+  }
+]);
